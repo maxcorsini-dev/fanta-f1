df76807a28c2c1761a55ed2d56eb3baa8ea5690f
Massimiliano Corsini <massimilianocorsini82@gmail.com>
2026-03-07 02:51:57 +0100

    https 2

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: false,
+    httpOnly: true,
     secure: process.env.NODE_ENV === 'production'
   }
 }));
